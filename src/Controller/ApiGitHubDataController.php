fc8d14a2651eb1478501b3494a2ab44814bdccf1
Vincent Chaillou <vincent.chaillou@another-dev.com>
2020-05-13 14:23:03 +0200

    Add some routes for somes basic api features

---
+++ src/Controller/ApiGitHubDataController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ApiGitHubDataController extends AbstractController
+{
+    public function getEventsCount(string $keyword, string $date, string $eventType = 'all')
+    {
+
+        return new JsonResponse();
+    }
+
+    public function getEventsList(string $keyword, string $date, string $eventType = 'all')
+    {
+
+        return new JsonResponse();
+    }
+
+    public function getEventDetails($id)
+    {
+
+        return new JsonResponse();
+    }
+
+}
